a939f9f009151f70973f7b042b1617c0a873d94a
AlexZhamoydin <alexzhamoydin@gmail.com>
2021-03-04 21:32:10 +0300

    ADDED: str, getter and initialization

--- bitset.py
+++ bitset.py
@@ -1,6 +1,13 @@
 class BitSet:
     def __init__(self):
-        pass
+        self._set = 0
+
+    def __str__(self):
+        return format(self._set, 'b').zfill(256)
+
+    @property
+    def set(self):
+        return self._set
 
     def add(self, char):
         pass
