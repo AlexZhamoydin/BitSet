0da2b3b49ca9ed2f50c08c482851680c5f5c2a59
AlexZhamoydin <alexzhamoydin@gmail.com>
2021-03-05 23:56:49 +0300

    ADDED: mock docstrings

--- bitset.py
+++ bitset.py
@@ -4,7 +4,6 @@ class BitSetException(Exception):
 
 
 class BitSet:
-
     def __init__(self):
         self._set = 0
 
@@ -14,19 +13,36 @@ class BitSet:
 
     @staticmethod
     def is_valid_char(char):
-        return 0 <= ord(char) < 256
+        """Checks if char belongs to ASCII table
+
+        :param char: char which value is checked
+        :return: True if char belongs to ASCII table.
+        """
+        if len(char) != 1:
+            raise BitSetException("Char expected")
+        return 0 <= ord(char) < 127
 
     @property
     def set(self):
         return self._set
 
     def add(self, char):
+        """Adds an element to set
+
+        :param char:
+        :return: True if
+        """
         if not BitSet.is_valid_char(char):
             raise BitSetException("Invalid character")
         else:
             pass
 
     def delete(self, char):
+        """Deletes an element from set
+
+        :param char:
+        :return:
+        """
         if not BitSet.is_valid_char(char):
             raise BitSetException("Invalid character")
         else:
@@ -34,4 +50,5 @@ class BitSet:
 
 
 a = BitSet()
+a.add('a')
 print(a)
